82ac34adb31c20ac216bc8e6857127cdcbafe1e4
kaileymonn <31671793+kaileymonn@users.noreply.github.com>
2018-12-09 17:19:26 -0800

    generate test matrix (in progress)

--- gen_numpy_files.py
+++ gen_numpy_files.py
@@ -8,12 +8,13 @@ import sys
 users = pd.read_csv('data/users.csv')
 businessPd = pd.read_csv("data/business.csv")
 trainReviews = pd.read_csv("new_train_reviews.csv")
-testPairs = pd.read_csv('new_test_queries.csv')
+old_testPairs = pd.read_csv("data/test_queries.csv")
+new_testPairs = pd.read_csv('new_test_queries.csv')
 
 def format_id(id):
     if id[0] == '-':
         return id[1:]
-    elif id[0] == '=' and id[1] == '-':
+    if id[0] == '=' and id[1] == '-':
         return id[2:]
     elif id[0] == '=' and id[1] != '-':
         return id[1:]
@@ -29,7 +30,7 @@ def createUserDict():
         useful = row['useful']
         funny = row['funny']
         cool = row['cool']
-        userDict[format_id(row['user_id'])] = [avgStars, numReviews, fans, useful, funny, cool]
+        userDict[row['user_id']] = [avgStars, numReviews, fans, useful, funny, cool]
     return userDict
 
 # Creates a vector for each business with the following features
@@ -39,7 +40,7 @@ def createBusinessDict():
     for index, row in businessPd.iterrows():
         avgStars = row['stars']
         numReviews = row['review_count']
-        businessDict[format_id(row['business_id'])] = [avgStars, numReviews]
+        businessDict[row['business_id']] = [avgStars, numReviews]
     return businessDict
 
 def createTrainingSet():
@@ -73,27 +74,21 @@ def createTrainingSet():
 
 def createTestSet():
     xTest = []
-    total_num = len(testPairs)
+    total_num_new = len(new_testPairs)
+    total_num_old = len(old_testPairs)
+    assert(total_num_new == total_num_old)
 
-    for index, row in testPairs.iterrows():
-        print("Generating test pair {0}/{1}".format(index+1, total_num), end="\r")
+    for index, row in old_testPairs.iterrows():
+        print("Generating test pair {0}/{1}".format(index+1, total_num_old), end="\r")
 
-        try:
-            uv = userDict[row['user_id']]
-        except:
-            # print("Can't find user {0}".format(row['user_id']))
-            # code.interact(local=locals())
-            continue
-        try:
-            bv = businessDict[row['business_id']]
-        except:
-            # print("Can't find biz {0}".format(row['business_id']))
-            # code.interact(local=locals())
-            continue
-        # Get sim score
-        sim_score = row['sim_score']
-        avg_stars_given = row['avg_stars_given']
-        avg_stars_received = row['avg_stars_received']
+        uv = userDict[row['user_id']]
+        bv = businessDict[row['business_id']]
+
+        new_row = new_testPairs.iloc[index]
+
+        sim_score = new_row['sim_score']
+        avg_stars_given = new_row['avg_stars_given']
+        avg_stars_received = new_row['avg_stars_received']
 
         # Concatenate uv and bv
         newVector = uv + bv + [sim_score] + [avg_stars_given] + [avg_stars_received]
@@ -104,12 +99,14 @@ print("Creating the user dictionary")
 userDict = createUserDict()
 print("Creating the business dictionary")
 businessDict = createBusinessDict()
-print("Creating the training set")
-X, Y = createTrainingSet()
+
+# print("Creating the training set")
+# X, Y = createTrainingSet()
+
 print("Creating the testing set")
 kaggleTest = createTestSet()
 
 
-np.save('xTrain', X)
-np.save('yTrain', Y)
+# np.save('xTrain', X)
+# np.save('yTrain', Y)
 np.save('kaggleTest', kaggleTest)
