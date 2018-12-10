f8a8fe8983abd87b75f47f504e5095ac17b4dd7b
kaileymonn <31671793+kaileymonn@users.noreply.github.com>
2018-12-09 16:39:36 -0800

    update

--- gen_numpy_files.py
+++ gen_numpy_files.py
@@ -8,6 +8,7 @@ import sys
 users = pd.read_csv('data/users.csv')
 businessPd = pd.read_csv("data/business.csv")
 trainReviews = pd.read_csv("new_train_reviews.csv")
+testPairs = pd.read_csv('new_test_queries.csv')
 
 def format_id(id):
     if id[0] == '-':
@@ -70,11 +71,45 @@ def createTrainingSet():
         xTrain.append(newVector)
     return np.asarray(xTrain), np.asarray(yTrain)
 
+def createTestSet():
+    xTest = []
+    total_num = len(testPairs)
+
+    for index, row in testPairs.iterrows():
+        print("Generating test pair {0}/{1}".format(index+1, total_num), end="\r")
+
+        try:
+            uv = userDict[row['user_id']]
+        except:
+            # print("Can't find user {0}".format(row['user_id']))
+            # code.interact(local=locals())
+            continue
+        try:
+            bv = businessDict[row['business_id']]
+        except:
+            # print("Can't find biz {0}".format(row['business_id']))
+            # code.interact(local=locals())
+            continue
+        # Get sim score
+        sim_score = row['sim_score']
+        avg_stars_given = row['avg_stars_given']
+        avg_stars_received = row['avg_stars_received']
+
+        # Concatenate uv and bv
+        newVector = uv + bv + [sim_score] + [avg_stars_given] + [avg_stars_received]
+        xTest.append(newVector)
+    return xTest
+
 print("Creating the user dictionary")
 userDict = createUserDict()
 print("Creating the business dictionary")
 businessDict = createBusinessDict()
 print("Creating the training set")
 X, Y = createTrainingSet()
+print("Creating the testing set")
+kaggleTest = createTestSet()
+
+
 np.save('xTrain', X)
 np.save('yTrain', Y)
+np.save('kaggleTest', kaggleTest)
